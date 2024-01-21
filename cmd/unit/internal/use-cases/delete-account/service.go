06944358cdb5a6c98cb2ed3d1fe8ba903a5b4a36
Anton Shchurov <rene.epicure@gmail.com>
2024-01-21 22:46:18 +0700

    refactor(use-case): DELETE-ACCOUNT — removed unused dependencies

--- cmd/unit/internal/use-cases/delete-account/service.go
+++ cmd/unit/internal/use-cases/delete-account/service.go
@@ -2,24 +2,17 @@ package delete_account
 
 type Repository interface {
 	Delete(accountID uint64) error
-	Archive(accountID uint64) error
-}
-
-type TransactionsRepository interface {
-	GetCountByAccount(accountID uint64) (uint64, error)
 }
 
 type DeleteAccountUseCase struct {
-	repo Repository
-
-	transactionsRepo TransactionsRepository
+	accounts Repository
 }
 
 // Execute
 // Прежде чем удалять счет, нужно убедиться, что он не используется в транзакциях.
 // Если используется, то архивируем счет, а не удаляем.
 func (u *DeleteAccountUseCase) Execute(accountID uint64) (bool, error) {
-	err := u.repo.Delete(accountID)
+	err := u.accounts.Delete(accountID)
 	if err != nil {
 		return false, err
 	}
